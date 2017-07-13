77ba4def5ed981f93a9a225fc08a3b3a319bd3b0
Ben Gift <ben@weaveup.com>
2017-07-12 18:13:36 -0700

    added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'x',
+  packages = ['x'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Simply get command line stdin by storing it as x',
+  author = 'Ben Gift',
+  author_email = 'benhgift@gmail.com    ',
+  url = 'https://github.com/Benhgift/x', # use the URL to the github repo
+  download_url = 'https://github.com/Benhgift/x/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['stdin', 'commandline', 'pipe'], # arbitrary keywords
+  classifiers = [],
+)
